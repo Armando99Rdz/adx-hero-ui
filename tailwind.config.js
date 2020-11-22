21ccde278a7f922866be964f0a01ab695d328abd
Armando Rodríguez <59629345+Armando99Rdz@users.noreply.github.com>
2020-11-22 00:36:24 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+        rose: colors.rose,
+        'true-gray': colors.trueGray,
+        'cool-gray': colors.coolGray,
+        fuchsia: colors.fuchsia
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  experimental: {
+    
+  }
+}
